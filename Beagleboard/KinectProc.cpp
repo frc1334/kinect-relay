e78309ef9fc3b7fede90235867dbe9d1cfe70671
Joseph Dillon <joe@w00t42.net>
2012-06-06 17:03:14 -0400

    build environment is messed

--- Beagleboard/KinectProc.cpp
+++ Beagleboard/KinectProc.cpp
@@ -8,5 +8,7 @@ KinectObject::KinectObject(freenect_context *_ctx, int _index) : Freenect::Freen
 void KinectObject::VideoCallback(void* _rgb, uint32_t timestamp)
 {
     mutex_video.lock();
+    uint8_t* frame = static_cast<uint8_t*>(_rgb);
+    delete frame;
     mutex_video.unlock();
 }
