8cf40cd3b959e89bcfc0256e872f89b5bea81dab
josephd <joe@w00t42.net>
2012-07-30 21:03:51 -0400

    added mutex classes

---
+++ Mutex.cpp
@@ -0,0 +1,34 @@
+/*
+ * Mutex.cpp
+ *
+ *  Created on: Jul 30, 2012
+ *      Author: joseph
+ */
+
+#include "Mutex.h"
+
+Mutex::Mutex()
+{
+	pthread_mutex_init(&mutex, NULL);
+}
+
+void Mutex::Lock()
+{
+	pthread_mutex_lock(&mutex);
+}
+
+void Mutex::Unlock()
+{
+	pthread_mutex_unlock(&mutex);
+}
+
+Mutex::ScopeMutex::ScopeMutex(Mutex &mutex)
+	: _mutex(mutex)
+{
+	_mutex.Lock();
+}
+
+Mutex::ScopeMutex::~ScopeMutex()
+{
+	_mutex.Unlock();
+}
---
+++ Mutex.h
@@ -0,0 +1,32 @@
+/*
+ * Mutex.h
+ *
+ *  Created on: Jul 30, 2012
+ *      Author: joseph
+ */
+
+#ifndef MUTEX_H_
+#define MUTEX_H_
+
+#include <pthread.h>
+
+class Mutex
+{
+public:
+	Mutex();
+	void Lock();
+	void Unlock();
+
+	class ScopeMutex
+	{
+	private:
+		Mutex &_mutex;
+	public:
+		ScopeMutex(Mutex &mutex);
+		~ScopeMutex();
+	};
+private:
+	pthread_mutex_t mutex;
+};
+
+#endif /* MUTEX_H_ */
