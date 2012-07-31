ada5947f8429f0b0ad1a7e4436bdd365ebd8e26b
josephd <joe@w00t42.net>
2012-07-30 21:46:58 -0400

    added support for basic configuration.  command line options later

--- kinect-relay.cpp
+++ kinect-relay.cpp
@@ -8,11 +8,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "KinectServer.h"
 #include <boost/asio.hpp>
+#include <libconfig.h++>
+#include "KinectServer.h"
+
+using namespace libconfig;
+
+struct ProgramConfig
+{
+	int port;
+} config;
 
 int main(void)
 {
+	Config cfg;
+
+	try
+	{
+		cfg.readFile("example.cfg");
+	}
+	catch(const FileIOException &fioex)
+	{
+		std::cerr << "I/O error while reading file." << std::endl;
+		return(EXIT_FAILURE);
+	}
+	catch(const ParseException &pex)
+	{
+		std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine() << " - " << pex.getError() << std::endl;
+		return(EXIT_FAILURE);
+	}
+
+	try
+	{
+		config.port = cfg.lookup("port");
+	}
+	catch(const SettingNotFoundException &nfex)
+	{
+		config.port = 4200;
+	}
+
 	try
 	{
 		boost::asio::io_service io_service;
