55cac421177d9274dc7a46d555db4ec116439e83
josephd <joe@w00t42.net>
2012-10-14 20:25:13 -0400

    client class reorganization; dividing client from robot code

---
+++ src/cRIO/KinectSubsystem.cpp
@@ -0,0 +1,18 @@
+/*
+ * KinectSubsystem.cpp
+ *
+ *  Created on: Oct 14, 2012
+ *      Author: joseph
+ */
+
+#include "KinectSubsystem.h"
+
+KinectSubsystem::KinectSubsystem() {
+	// TODO Auto-generated constructor stub
+
+}
+
+KinectSubsystem::~KinectSubsystem() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/cRIO/KinectSubsystem.h
@@ -0,0 +1,17 @@
+/*
+ * KinectSubsystem.h
+ *
+ *  Created on: Oct 14, 2012
+ *      Author: joseph
+ */
+
+#ifndef KINECTSUBSYSTEM_H_
+#define KINECTSUBSYSTEM_H_
+
+class KinectSubsystem {
+public:
+	KinectSubsystem();
+	virtual ~KinectSubsystem();
+};
+
+#endif /* KINECTSUBSYSTEM_H_ */
